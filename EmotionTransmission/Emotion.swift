b0d9087bbd7ada4777ef2f6f62c6f977f408bd76
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2018-01-28 16:55:04 -0200

    Working POC

--- EmotionTransmission/Emotion.swift
+++ EmotionTransmission/Emotion.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 enum Emotion: String {
     case anger
     case contempt
@@ -7,4 +9,21 @@ enum Emotion: String {
     case neutral
     case sadness
     case surprise
+
+    static func random() -> Emotion {
+        let emotions = [
+            Emotion.anger.rawValue,
+            Emotion.contempt.rawValue,
+            Emotion.disgust.rawValue,
+            Emotion.fear.rawValue,
+            Emotion.happiness.rawValue,
+            Emotion.neutral.rawValue,
+            Emotion.sadness.rawValue,
+            Emotion.surprise.rawValue,
+        ]
+
+        let index = Int(arc4random_uniform(UInt32(emotions.count)))
+
+        return Emotion(rawValue: emotions[index])!
+    }
 }
--- EmotionTransmission/ViewController.swift
+++ EmotionTransmission/ViewController.swift
@@ -1,15 +1,57 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet var imageVIew: UIImageView!
+    @IBOutlet var target: UILabel!
+    @IBOutlet var status: UILabel!
+
+    var currentEmotion: Emotion?
+
+    func newEmotion() {
+        imageVIew.image = nil
+        currentEmotion = Emotion.random()
+        target.text = "Target: " + currentEmotion!.rawValue
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let image = UIImage(named: "test")!
-        Requester().classify(image: image) { (val, err) in
-            dump(val)
-            print(val?.strongestEmotion())
-            print(err)
+        newEmotion()
+    }
+
+    @IBAction func openCameraButton(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        } else if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
         }
     }
 
+    @IBAction func tryAgain(sender: AnyObject) {
+        newEmotion()
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageVIew.image = image
+        status.text = "Processing..."
+        Requester().classify(image: image) { (response, error) in
+            let strongest = response!.strongestEmotion()
+            if strongest == self.currentEmotion {
+                self.status.text = "Good job!"
+            } else {
+                self.status.text = "You got " + strongest.rawValue
+            }
+        }
+        dismiss(animated:true, completion: nil)
+    }
+    
 }
